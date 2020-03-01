2a435ec03b3620184fb6612a6fbd368f4f45546c
Xuan Tang <txstc55@gmail.com>
2020-03-01 16:02:44 -0500

    added args

--- plot.py
+++ plot.py
@@ -1,33 +1,39 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+
 
 results = []
-with open('result_datas/all_result_demo1.json', 'r') as j:
-    results = json.load(j)
+if len(sys.argv) == 2:
+    with open(sys.argv[1], 'r') as j:
+        results = json.load(j)
+else:
+    with open('result_datas/all_result.json', 'r') as j:
+        results = json.load(j)
 
 assemble = [float(x["ASSEMBLE"])/100 for x in results]
 compute = [float(x["COMPUTE"])/100 for x in results]
 solve = [float(x["SOLVE"])/100 for x in results]
 
-total_time =[assemble[i]+compute[i]+solve[i] for i in range(5)]
+# total_time =[assemble[i]+compute[i]+solve[i] for i in range(5)]
 
-bars = np.add(assemble, compute).tolist()
+bars = np.add(solve, compute).tolist()
 
 N = 5
 
 ind = np.arange(N)    # the x locations for the groups
 width = 0.35       # the width of the bars: can also be len(x) sequence
 
-p1 = plt.bar(ind, assemble, width)
-p2 = plt.bar(ind, compute, width, bottom=assemble)
-p3 = plt.bar(ind, solve, width, bottom=bars)
+p1 = plt.bar(ind, solve, width)
+p2 = plt.bar(ind, compute, width, bottom=solve)
+p3 = plt.bar(ind, assemble, width, bottom=bars)
 
 plt.ylabel('Time')
 plt.title('Method')
 plt.xticks(ind, [x["name"] for x in results])
 plt.yticks(np.arange(0, 81, 10))
-plt.legend((p1[0], p2[0], p3[0]), ('Assemble', 'Compute', 'Solve'))
+plt.legend((p1[0], p2[0], p3[0]), ('Solve', 'Compute', 'Assemble'))
 
 
 for r1, r2, r3 in zip(p1, p2, p3):

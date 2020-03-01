3d477c4bed1202217735422efbff8f7ecf49f733
Xuan Tang <txstc55@gmail.com>
2020-03-01 16:04:04 -0500

    added also print

--- include/igl/test_record_util.cpp
+++ include/igl/test_record_util.cpp
@@ -1,5 +1,5 @@
 #include "test_record_util.h"
-
+#include <iostream>
 namespace igl
 {
 IGL_INLINE void write_to_file(std::ofstream &file, std::string message, double time, bool first_call)
@@ -7,6 +7,7 @@ IGL_INLINE void write_to_file(std::ofstream &file, std::string message, double t
     if (!first_call)
     {
         file << message << ": " << time << "\n";
+        std::cout << message << ": " << time << "\n";
     }
 }
 } // namespace igl

44030c00d054d9827f988a1562d0c8875232ff77
Xuan Tang <txstc55@gmail.com>
2020-03-25 14:41:02 -0400

    added for dense matrix executor

--- include/igl/inline_expansion/NumericExecutor.cpp
+++ include/igl/inline_expansion/NumericExecutor.cpp
@@ -3,10 +3,11 @@
 
 using namespace std;
 
-namespace ie {
+namespace ie
+{
 NumericExecutor::NumericExecutor() {}
 
-NumericExecutor::NumericExecutor(Eigen::SparseMatrix<ie::NumericType, Eigen::RowMajor>& R, size_t gap)
+NumericExecutor::NumericExecutor(Eigen::SparseMatrix<ie::NumericType, Eigen::RowMajor> &R, size_t gap)
 {
     this->gap = gap;
     this->th = NumericVisitorTreeHashing(R.nonZeros());
@@ -22,8 +23,9 @@ NumericExecutor::NumericExecutor(Eigen::SparseMatrix<ie::NumericType, Eigen::Row
     this->th.accept(this->tg);
     this->tg.compile_file();
     this->tg.link_functions();
+    cout << "Linking function completed\n";
 }
-NumericExecutor::NumericExecutor(Eigen::SparseMatrix<ie::NumericType, Eigen::ColMajor>& R, size_t gap)
+NumericExecutor::NumericExecutor(Eigen::SparseMatrix<ie::NumericType, Eigen::ColMajor> &R, size_t gap)
 {
     this->gap = gap;
     this->th = NumericVisitorTreeHashing(R.nonZeros());
@@ -39,17 +41,32 @@ NumericExecutor::NumericExecutor(Eigen::SparseMatrix<ie::NumericType, Eigen::Col
     this->th.accept(this->tg);
     this->tg.compile_file();
     this->tg.link_functions();
+    cout << "Linking function completed\n";
 }
 
+NumericExecutor::NumericExecutor(Eigen::Matrix<ie::NumericType, Eigen::Dynamic, Eigen::Dynamic> &R, size_t gap)
+{
+    int matrix_size = R.rows() * R.cols();
+    this->gap = gap;
+    this->th = NumericVisitorTreeHashing(matrix_size);
+    tbb::parallel_for(size_t(0), size_t(matrix_size), [&](size_t i) {
+        R(i % R.rows(), i / R.rows()).accept(this->th, i);
+    });
+    cout << "Group by tree type method chosen\n";
+    this->tg = TreeToFileVisitorGroupByFunction(gap);
+    this->th.accept(this->tg);
+    this->tg.compile_file();
+    this->tg.link_functions();
+    cout << "Linking function completed\n";
+}
 
-
-void NumericExecutor::ExecuteSingle(const vector<vector<double>>& data, vector<double>& result)
+void NumericExecutor::ExecuteSingle(const vector<vector<double>> &data, vector<double> &result)
 {
     this->tg.Execute_single(this->tg.reordered_data_id, this->tg.reordered_result_position, data, result);
 }
 
-void NumericExecutor::ExecuteMulti(const vector<vector<double>>& data, vector<double>& result)
+void NumericExecutor::ExecuteMulti(const vector<vector<double>> &data, vector<double> &result)
 {
     this->tg.Execute_multi(this->tg.reordered_data_id, this->tg.reordered_result_position, data, result);
 }
-}
+} // namespace ie
--- include/igl/inline_expansion/NumericExecutor.hpp
+++ include/igl/inline_expansion/NumericExecutor.hpp
@@ -19,6 +19,7 @@ public:
     NumericExecutor();
     NumericExecutor(Eigen::SparseMatrix<ie::NumericType, Eigen::RowMajor>& R, size_t gap);
     NumericExecutor(Eigen::SparseMatrix<ie::NumericType, Eigen::ColMajor>& R, size_t gap);
+    NumericExecutor(Eigen::Matrix<ie::NumericType, Eigen::Dynamic, Eigen::Dynamic>& R, size_t gap);
     void ExecuteSingle(const std::vector<std::vector<double>>& data, std::vector<double>& result);
     void ExecuteMulti(const std::vector<std::vector<double>>& data, std::vector<double>& result);
 };

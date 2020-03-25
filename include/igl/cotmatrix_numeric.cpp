bef969827ff7ccd5c30128807112fe001a68ed6f
Xuan Tang <txstc55@gmail.com>
2020-03-25 01:19:05 -0400

    first trial for cotmatrix numeric type

---
+++ include/igl/cotmatrix_numeric.cpp
@@ -0,0 +1,103 @@
+#include "cotmatrix_numeric.h"
+#include "inline_expansion/utils.hpp"
+namespace igl
+{
+
+IGL_INLINE void squared_edge_lengths_numeric(Eigen::Matrix<ie::NumericType, Eigen::Dynamic, Eigen::Dynamic> &V_numeric, const Eigen::MatrixXi &F, Eigen::Matrix<ie::NumericType, Eigen::Dynamic, Eigen::Dynamic> &L)
+{
+    using namespace std;
+    const int m = F.rows();
+    L.resize(m, 3);
+    for (int i = 0; i < m; i++)
+    {
+        L(i, 0) = (V_numeric(F(i, 1), 0) - V_numeric(F(i, 2), 0)) * (V_numeric(F(i, 1), 0) - V_numeric(F(i, 2), 0)) + (V_numeric(F(i, 1), 1) - V_numeric(F(i, 2), 1)) * (V_numeric(F(i, 1), 1) - V_numeric(F(i, 2), 1)) + (V_numeric(F(i, 1), 2) - V_numeric(F(i, 2), 2)) * (V_numeric(F(i, 1), 2) - V_numeric(F(i, 2), 2));
+        L(i, 1) = (V_numeric(F(i, 2), 0) - V_numeric(F(i, 0), 0)) * (V_numeric(F(i, 2), 0) - V_numeric(F(i, 0), 0)) + (V_numeric(F(i, 2), 1) - V_numeric(F(i, 0), 1)) * (V_numeric(F(i, 2), 1) - V_numeric(F(i, 0), 1)) + (V_numeric(F(i, 2), 2) - V_numeric(F(i, 0), 2)) * (V_numeric(F(i, 2), 2) - V_numeric(F(i, 0), 2));
+        L(i, 2) = (V_numeric(F(i, 0), 0) - V_numeric(F(i, 1), 0)) * (V_numeric(F(i, 0), 0) - V_numeric(F(i, 1), 0)) + (V_numeric(F(i, 0), 1) - V_numeric(F(i, 1), 1)) * (V_numeric(F(i, 0), 1) - V_numeric(F(i, 1), 1)) + (V_numeric(F(i, 0), 2) - V_numeric(F(i, 1), 2)) * (V_numeric(F(i, 0), 2) - V_numeric(F(i, 1), 2));
+    }
+}
+
+IGL_INLINE void doublearea_numeric(Eigen::Matrix<ie::NumericType, Eigen::Dynamic, Eigen::Dynamic> &l, Eigen::Matrix<ie::NumericType, Eigen::Dynamic, Eigen::Dynamic> &A)
+{
+    A.resize(l.rows(), 1);
+    for (int i = 0; i < l.rows(); i++)
+    {
+        A(i, 0) = 0.5 * ((l(i, 0) + (l(i, 1) + l(i, 2))) *
+                         (l(i, 2) - (l(i, 0) - l(i, 1))) *
+                         (l(i, 2) + (l(i, 0) - l(i, 1))) *
+                         (l(i, 0) + (l(i, 1) - l(i, 2))))
+                            .sqrt();
+    }
+}
+
+IGL_INLINE void cotmatrix_entries_numeric(Eigen::Matrix<ie::NumericType, Eigen::Dynamic, Eigen::Dynamic> &V_numeric, const Eigen::MatrixXi &F, Eigen::Matrix<ie::NumericType, Eigen::Dynamic, Eigen::Dynamic> &C)
+{
+    using namespace std;
+    using namespace Eigen;
+    Matrix<ie::NumericType, Dynamic, Dynamic> l2;
+    // Compute the squared edge length
+    squared_edge_lengths_numeric(V_numeric, F, l2);
+    // Compute Edge lengths
+    Matrix<ie::NumericType, Dynamic, Dynamic> l;
+    l.resize(l2.rows(), 3);
+    for (int i = 0; i < l.rows(); i++)
+    {
+        for (int j = 0; j < 3; j++)
+            l(i, j) = l2(i, j).sqrt();
+    }
+
+    Matrix<ie::NumericType, Dynamic, Dynamic> dblA;
+    doublearea_numeric(l, dblA);
+    C.resize(F.rows(), 3);
+    for (int i = 0; i < F.rows(); i++)
+    {
+        C(i, 0) = (l2(i, 1) + l2(i, 2) - l2(i, 0)) / dblA(i) / 4.0;
+        C(i, 1) = (l2(i, 2) + l2(i, 0) - l2(i, 1)) / dblA(i) / 4.0;
+        C(i, 2) = (l2(i, 0) + l2(i, 1) - l2(i, 2)) / dblA(i) / 4.0;
+    }
+}
+
+IGL_INLINE void cotmatrix_numeric(COTMATRIXData &cd)
+{
+    using namespace Eigen;
+    using namespace std;
+    if (cd.first_called)
+    {
+        cd.datas.resize(1);
+        cd.datas[0].resize(cd.V.rows() * cd.V.cols());
+        SparseMatrix<ie::NumericType> L;
+        L.resize(cd.V.rows(), cd.V.rows());
+        Matrix<int, Dynamic, 2> edges;
+        L.reserve(10 * cd.V.rows());
+        edges.resize(3, 2);
+        edges << 1, 2,
+            2, 0,
+            0, 1;
+        Matrix<ie::NumericType, Dynamic, Dynamic> C;
+        Matrix<ie::NumericType, Dynamic, Dynamic> V_numeric;
+        V_numeric.resize(cd.V.rows(), cd.V.cols());
+        V_numeric = ie::to_dense_numeric(cd.V, 0);
+        cotmatrix_entries_numeric(V_numeric, cd.F, C);
+
+        vector<Triplet<ie::NumericType>> IJV;
+        IJV.reserve(cd.F.rows() * edges.rows() * 4);
+        // Loop over triangles
+        for (int i = 0; i < cd.F.rows(); i++)
+        {
+            // loop over edges of element
+            for (int e = 0; e < edges.rows(); e++)
+            {
+                int source = cd.F(i, edges(e, 0));
+                int dest = cd.F(i, edges(e, 1));
+                IJV.push_back(Triplet<ie::NumericType>(source, dest, C(i, e)));
+                IJV.push_back(Triplet<ie::NumericType>(dest, source, C(i, e)));
+                IJV.push_back(Triplet<ie::NumericType>(source, source, ie::NumericType(0.0) - C(i, e)));
+                IJV.push_back(Triplet<ie::NumericType>(dest, dest, ie::NumericType(0.0) - C(i, e)));
+            }
+        }
+        L.setFromTriplets(IJV.begin(), IJV.end());
+        cd.ex = ie::NumericExecutor(L, 0);
+    }
+    cd.datas[0].assign(cd.V.data(), cd.V.data() + cd.V.rows() * cd.V.cols());
+    cd.ex.ExecuteMulti(cd.datas, cd.result);
+}
+} // namespace igl
